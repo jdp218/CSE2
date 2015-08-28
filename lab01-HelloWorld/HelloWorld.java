275c65c7c3d90589137d67d7df9bf93e58ef601c
jpsy <jdp218@lehigh.edu>
2015-08-28 15:58:22 +0000

    lab 01

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,26 @@
+////////////////////////////////////////
+//Jordan Parks
+//Hello World Java Program, lab 01
+// first compile the program
+// javac Helloworld.Java
+// run the program
+// java HelloWorld//
+
+//Define a class
+public class HelloWorld{
+    
+    //add main method
+    public static void main(String[] args) {
+        
+//print out the statement with the box around it
+    System.out.println("########################");
+    System.out.println("########################");
+    System.out.println("#                      #");
+    System.out.println("#  Hello World 2014!!  #");
+    System.out.println("#                      #");
+    System.out.println("########################");
+    System.out.println("########################");
+    System.out.println("test");
+    
+    }
+}
