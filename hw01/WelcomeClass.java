1b476d6c0e0865e7f65d26e4a6ce7fca613339da
jpsy <jdp218@lehigh.edu>
2015-08-31 13:38:09 +0000

    updated homework

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,24 @@
+////////////////////////////////////////
+//Jordan Parks
+//Hello Class Java Program, Homework 01
+
+//Define a class
+public class WelcomeClass{
+    
+    //add main method
+    public static void main(String[] args) {
+        
+//this is where my 'printout' goes
+    System.out.println("  -----------");
+    System.out.println("  | WELCOME |");
+    System.out.println("  -----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("<-J--D--P--2--1--8->");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v");
+    System.out.println("Over the summer I worked for a real estate management company.");
+    System.out.println("I also got some college math credits.");
+
+    }
+}
